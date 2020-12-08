bb81c30e7e45d424eb73447cf855c154ef72cea8
owen-gervais <60107914+owen-gervais@users.noreply.github.com>
2020-12-08 15:31:34 -0500

    Create orientation_publisher.py

---
+++ Spike_ROS2/Spike/orientation_publisher.py
@@ -0,0 +1,9 @@
+import hub, import utime
+
+uart = hub.port.B
+uart.mode(hub.port.MODE_FULL_DUPLEX)
+uart.baud(115200)
+
+while(True):
+    uart.write(str(hub.motion.orientation()) + '\n')
+    utime.sleep(0.01)
