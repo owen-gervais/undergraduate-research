b39e378898d27c2c37d6459d51f75569d5b4860b
owen-gervais <60107914+owen-gervais@users.noreply.github.com>
2020-12-14 15:45:40 -0500

    Add files via upload

---
+++ Spike_ROS2/ROS2/bbot/launch/start.launch.py
@@ -0,0 +1,65 @@
+                                                        # you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import os
+import xacro
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+             )
+
+
+    bbot_path = os.path.join(
+        get_package_share_directory('bbot'))
+
+    xacro_file = os.path.join(bbot_path,
+                              'sdf',
+                              'robot.xacro.sdf')
+
+    sdf_file = os.path.join(bbot_path,
+                              'sdf',
+                              'robot.sdf')
+
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    
+    # Open a file
+    fd = os.open( sdf_file, os.O_RDWR|os.O_CREAT )
+    os.ftruncate(fd, 0)
+    os.lseek(fd, 0, os.SEEK_SET)
+    b = str.encode(doc.toxml())
+    os.write(fd, b)
+
+    # Close opened file
+    os.close(fd)
+
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+        arguments=['-entity', 'bbot', '-file', sdf_file,
+        '-x', '0.061475' ,'-y', '-0.058621', '-z', '0.058962', '-R', '-0.000043', '-P','0.563955', '-Y','0.425422'],
+        output='screen')
+
+
+    return LaunchDescription([
+        gazebo,
+        spawn_entity,
+    ])
+
